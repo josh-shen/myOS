e8103549468fb21593c11d9475ecdff1b283e715
jxs <joshausneh@gmail.com>
2025-05-28 17:43:56 +0000

    Identify usable and reserved regions of memory

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -21,15 +21,50 @@ void kernel_main(void) {
 	terminal_initialize();
 	init_keyboard();
 
+	// Memory management
 	printf("Flags: 0x%x\n", mbi->flags); 
 	printf("Mem lower: 0x%x\n", mbi->mem_lower);
 	printf("Mem higher: 0x%x\n", mbi->mem_upper);
 	
-	printf("mmap length: 0x%x\n", mbi->mmap_length);
-	printf("mmap addr: 0x%x\n", mbi->mmap_addr);
+	// Check if bit 6 of flags is set
+	if (mbi->flags & (1 << 6)) {
+		printf("mmap length: 0x%x\n", mbi->mmap_length);
+		printf("mmap addr: 0x%x\n", mbi->mmap_addr);
 
-	// Pointer to first memory map entry
-	mmap_entry_t *mmap_entry = (mmap_entry_t *)(uintptr_t)mbi->mmap_addr;
+		// Pointer to first memory map entry
+		mmap_entry_t *mmap_entry = (mmap_entry_t *)(uintptr_t)mbi->mmap_addr;
+
+		uint64_t total_memory = 0x0;
+
+		while((uintptr_t)mmap_entry < (uintptr_t)mbi->mmap_addr + mbi->mmap_length) {
+			uint64_t base_addr = ((uint64_t)mmap_entry->base_addr_high << 32) | mmap_entry->base_addr_low;
+			uint64_t length = ((uint64_t)mmap_entry->length_high << 32) | mmap_entry->length_low;
+
+			uint32_t type = mmap_entry->type;
+			
+			if (type == 0x1) {
+				printf("addr %x\n", base_addr);
+				printf("0x%x%x bytes of usable memory at address 0x%x%x \n", mmap_entry->length_high, mmap_entry->length_low, mmap_entry->base_addr_high, mmap_entry->base_addr_low);
+			} else if (type == 0x2) {
+				printf("reserved\n");
+			} else if (type == 0x3) {
+				printf("0x%x%x bytes of ACPI reclaimable memory at address 0x%x%x \n", mmap_entry->length_high, mmap_entry->length_low, mmap_entry->base_addr_high, mmap_entry->base_addr_low);
+			} else if (type == 0x4) {
+				printf("Reserved\n");
+			} else if (type == 0x5) {
+				printf("defective\n");
+			} else {
+				printf("other\n"); // There should never be "other" types
+			}			
+			printf("\n");
+			
+			// Add memory map entry size + size field
+			mmap_entry = (mmap_entry_t *)((uintptr_t)mmap_entry + mmap_entry->size + sizeof(mmap_entry->size));
+		}
+	} else {
+		printf("Memory map not available\n");
+		
+	}
 	
 	//printf("Hello, kernel World!\n");
 	//printf("%d %d %x\n", 0, 123456789, 1234);
