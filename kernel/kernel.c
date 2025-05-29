cc6420d2a2f0f2a2bbee3064d6925a4c4a6bd62a
jxs <joshausneh@gmail.com>
2025-05-29 21:59:05 +0000

    Memory bitmap

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,71 +1,24 @@
 #include <stdio.h>
-#include <stdint.h>
 
-#include <multiboot.h>
+#include <memory.h>
+#include <tty.h>
 #include <interrupts.h>
 #include <timer.h>
-#include <tty.h>
 #include <keyboard.h>
 
-extern multiboot_info_t *multiboot_info_ptr;
-
 void kernel_main(void) {
-	multiboot_info_t *mbi = multiboot_info_ptr;
-	
+	terminal_initialize();
+		
 	init_gdt();
 	init_idt();
 	init_isr();
 	init_irq(); // Interrupts enabled
 
+	init_memory();
+	
 	//init_timer(1);
-	terminal_initialize();
-	init_keyboard();
+	//init_keyboard();
 
-	// Memory management
-	printf("Flags: 0x%x\n", mbi->flags); 
-	printf("Mem lower: 0x%x\n", mbi->mem_lower);
-	printf("Mem higher: 0x%x\n", mbi->mem_upper);
-	
-	// Check if bit 6 of flags is set
-	if (mbi->flags & (1 << 6)) {
-		printf("mmap length: 0x%x\n", mbi->mmap_length);
-		printf("mmap addr: 0x%x\n", mbi->mmap_addr);
-
-		// Pointer to first memory map entry
-		mmap_entry_t *mmap_entry = (mmap_entry_t *)(uintptr_t)mbi->mmap_addr;
-
-		uint64_t total_memory = 0x0;
-
-		while((uintptr_t)mmap_entry < (uintptr_t)mbi->mmap_addr + mbi->mmap_length) {
-			uint64_t base_addr = ((uint64_t)mmap_entry->base_addr_high << 32) | mmap_entry->base_addr_low;
-			uint64_t length = ((uint64_t)mmap_entry->length_high << 32) | mmap_entry->length_low;
-
-			uint32_t type = mmap_entry->type;
-			
-			if (type == 0x1) {
-				printf("addr %x\n", base_addr);
-				printf("0x%x%x bytes of usable memory at address 0x%x%x \n", mmap_entry->length_high, mmap_entry->length_low, mmap_entry->base_addr_high, mmap_entry->base_addr_low);
-			} else if (type == 0x2) {
-				printf("reserved\n");
-			} else if (type == 0x3) {
-				printf("0x%x%x bytes of ACPI reclaimable memory at address 0x%x%x \n", mmap_entry->length_high, mmap_entry->length_low, mmap_entry->base_addr_high, mmap_entry->base_addr_low);
-			} else if (type == 0x4) {
-				printf("Reserved\n");
-			} else if (type == 0x5) {
-				printf("defective\n");
-			} else {
-				printf("other\n"); // There should never be "other" types
-			}			
-			printf("\n");
-			
-			// Add memory map entry size + size field
-			mmap_entry = (mmap_entry_t *)((uintptr_t)mmap_entry + mmap_entry->size + sizeof(mmap_entry->size));
-		}
-	} else {
-		printf("Memory map not available\n");
-		
-	}
-	
 	//printf("Hello, kernel World!\n");
 	//printf("%d %d %x\n", 0, 123456789, 1234);
 	// asm volatile ("int $0x3");
---
+++ kernel/memory/memory.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include <memory.h>
+#include <multiboot.h>
+
+static void mark_free(uint64_t, uint64_t);
+static void mark_used(uint64_t, uint64_t);
+
+extern multiboot_info_t *multiboot_info_ptr;
+
+#define FRAME_SIZE 4096                     // 4 KB pages
+#define MAX_FRAMES 32752                    // Total frames of memory
+uint8_t pmm_bitmap[MAX_FRAMES / 8] = {1};   // Each bit of a byte represents a frame
+
+static void mark_free(uint64_t base, uint64_t len) {
+    for (uint64_t curr_frame = base; curr_frame < base + len; curr_frame += FRAME_SIZE) {
+        uint16_t frame_num = curr_frame / 4096;
+        uint16_t byte_index = frame_num / 8;
+        uint8_t bit_index = frame_num % 8;
+
+        pmm_bitmap[byte_index] &= ~(1 << bit_index);
+    }
+}
+
+static void mark_used(uint64_t base, uint64_t len) {
+    for (uint64_t curr_frame = base; curr_frame < base + len; curr_frame += FRAME_SIZE) {
+        uint16_t frame_num = curr_frame / 4096;
+        uint16_t byte_index = frame_num / 8;
+        uint8_t bit_index = frame_num % 8;
+
+        pmm_bitmap[byte_index] |= (1 << bit_index);
+    }
+}
+
+void init_memory() {
+    multiboot_info_t *mbi = multiboot_info_ptr;
+	    
+    // Check if bit 6 of flags is set
+    if (!(mbi->flags & (1 << 6))) {
+        printf("Memory map not available\n");
+        return;
+    }
+
+    // Pointer to first memory map entry
+    mmap_entry_t *mmap_entry = (mmap_entry_t *)(uintptr_t)mbi->mmap_addr;
+
+    while((uintptr_t)mmap_entry < (uintptr_t)mbi->mmap_addr + mbi->mmap_length) {
+        uint64_t base_addr = ((uint64_t)mmap_entry->base_addr_high << 32) | mmap_entry->base_addr_low;
+        uint64_t length = ((uint64_t)mmap_entry->length_high << 32) | mmap_entry->length_low;
+        
+        if (mmap_entry->type == 1) {
+            mark_free(base_addr, length);
+        } else if (mmap_entry->type == 3) {
+            // TODO: This is ACPI reclimable memory, ACPI data needs to be processed first
+            mark_free(base_addr, length);
+        } else if ((mmap_entry->type == 2) || (mmap_entry->type == 4) || (mmap_entry->type == 5)) {
+            mark_used(base_addr, length);
+        }
+
+        // Add memory map entry size + size field
+        mmap_entry = (mmap_entry_t *)((uintptr_t)mmap_entry + mmap_entry->size + sizeof(mmap_entry->size));
+    }
+    printf("%x", pmm_bitmap[19]);
+
+    // bitmap size is 4094 bytes (32752 / 8), < 4KB. The first frame of the bitmap goes to the bitmap itself
+}
