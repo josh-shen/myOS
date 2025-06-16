e79b795e601ff1eeb7971f8d2f2940f5cb41c6aa
jxs <joshausneh@gmail.com>
2025-06-16 22:25:28 +0000

    Switched from bitmap to buddy allocator. TODO: map free memory before marking

--- kernel/memory/pmm.c
+++ kernel/memory/pmm.c
@@ -1,54 +1,96 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
+#include <stddef.h>
 
 #include <memory.h>
 #include <multiboot.h>
 
-static void mark_free(uint64_t, uint64_t);
-static void mark_used(uint64_t, uint64_t);
+static void mark_free(uintptr_t, uintptr_t);
+static uint8_t get_order(uintptr_t);
 
 extern char kernel_start;
 extern char kernel_end;
 
-#define FRAME_SIZE 4096             // 4 KiB pages
-#define TOTAL_FRAMES 1048576        // 4 GiB memory
+#define MAX_ORDER 11
 
-uint8_t pmm_bitmap[TOTAL_FRAMES / 8]__attribute__((section(".pmm_bitmap")));
-struct physical_memory_manager {
-    uint32_t total_frames;
-    uint32_t free_frames;
+struct partition {
+    struct partition *prev;
+    struct partition *next;
 };
-typedef struct physical_memory_manager pmm_t;
+typedef struct partition partition_t;
 
-static pmm_t pmm;
+partition_t *free_lists[MAX_ORDER]__attribute__((section(".free_lists")));
 
-static void mark_free(uint64_t base, uint64_t len) {
-    for (uint64_t curr_address = base; curr_address < base + len; curr_address += FRAME_SIZE) {
-        uint16_t frame_num = curr_address / FRAME_SIZE;
-        uint16_t byte_index = frame_num / 8;
-        uint8_t bit_index = frame_num % 8;
+// Known used regions 
+#define NUM_USED_REGIONS 2
+uintptr_t used_regions[NUM_USED_REGIONS][2] = {
+    {(uintptr_t)&kernel_start, (uintptr_t)&kernel_end}, // Kernel, includes kernel stack
+    {0xB80000, 8000}                                    // VGA memory
+};
 
-        pmm_bitmap[byte_index] &= ~(1 << bit_index);
-        pmm.free_frames += 1;
+static uint8_t get_order(uintptr_t length) {
+    for (int n = MAX_ORDER - 1; n >= 0; n--) {
+        if ((2 << (n + 11)) < length) return n;
+    }
+    return 0;
+}
+
+static void mark_free(uintptr_t base, uintptr_t len) {
+    while (len >= 4096) {
+        uint8_t order = get_order(len);
+        uint32_t partition_size = 2 << (order + 11);
+        
+        partition_t *partition = (partition_t *)(uintptr_t)base + 0xC0000000;
+        if (free_lists[order] == NULL) {
+            partition->prev = NULL;
+            partition->next = NULL;
+            free_lists[order] = partition;
+        } else {
+            partition->next = free_lists[order];
+            partition->prev = NULL;
+
+            free_lists[order]->prev = partition;
+            free_lists[order] = partition;
+        }
+
+        base += partition_size;
+        len -= partition_size;
     }
 }
 
-static void mark_used(uint64_t base, uint64_t len) {
-    for (uint64_t curr_address = base; curr_address < base + len; curr_address += FRAME_SIZE) {
-        uint16_t frame_num = curr_address / FRAME_SIZE;
-        uint16_t byte_index = frame_num / 8;
-        uint8_t bit_index = frame_num % 8;
+static void filter(uintptr_t base, uintptr_t length) {
+    for (int i = 0; i < NUM_USED_REGIONS; i++) {
+        uintptr_t used_end = used_regions[i][0] + used_regions[i][1];
+        uintptr_t end = base + length;
 
-        pmm_bitmap[byte_index] |= (1 << bit_index);
+        // Ignore blocks outside current block
+        if (used_end <= base || used_regions[i][0] >= end) continue;
+
+        if (used_regions[i][0] == base && used_end == end) {
+            // Used region covers entire block
+            return;
+        }
+        if (used_regions[i][0] == base && used_end < end) {
+            // Used region is at the very start of the block
+            filter(used_end, end - used_end);
+            return;
+        }
+        if (used_regions[i][0] > base && used_end == end) {
+            // Used region is at the very end of the block
+            filter(base, used_regions[i][0] - base);
+            return;
+        }
+        if (used_regions[i][0] > base && used_end < end) {
+            filter(base, used_regions[i][0] - base);
+            filter(used_end, end - used_end);
+            return;
+        }
     }
+    printf("free: %x %x\n", base, length);
+    //mark_free(base, length);
 }
 
-void pmm_init(uint32_t multiboot_info_ptr) {
-    memset(pmm_bitmap, 0xFF, sizeof(pmm_bitmap));
-    pmm.total_frames = TOTAL_FRAMES;
-    pmm.free_frames = 0;
-    
+void pmm_init(uint32_t multiboot_info_ptr) {    
     multiboot_info_t *mbi = (multiboot_info_t*)multiboot_info_ptr;
 
     // Check if bit 6 of flags is set for mmap_*
@@ -57,34 +99,34 @@ void pmm_init(uint32_t multiboot_info_ptr) {
         return;
     }
 
+    for (uint8_t i = 0; i < MAX_ORDER; i++) {
+        free_lists[i] = NULL;
+    }
+
     // Pointer to first memory map entry
     mmap_entry_t *mmap_entry = (mmap_entry_t *)mbi->mmap_addr;
 
     while((uintptr_t)mmap_entry < mbi->mmap_addr + mbi->mmap_length) {
-        uint64_t base_addr = ((uint64_t)mmap_entry->base_addr_high << 32) | mmap_entry->base_addr_low;
-        uint64_t length = ((uint64_t)mmap_entry->length_high << 32) | mmap_entry->length_low;
-        printf("%x, %x, %x\n",mmap_entry->base_addr_low, mmap_entry->length_low, mmap_entry->type);
+        uintptr_t base_addr = ((uint64_t)mmap_entry->base_addr_high << 32) | mmap_entry->base_addr_low;
+        uintptr_t length = ((uint64_t)mmap_entry->length_high << 32) | mmap_entry->length_low;
+
         if (mmap_entry->type == 1) {
-            printf("free\n");
-            mark_free(base_addr, length);
+            filter(base_addr, length);
         } else if (mmap_entry->type == 3) {
             // TODO: This is ACPI reclimable memory, ACPI data needs to be processed first
-            mark_free(base_addr, length);
-        } else if ((mmap_entry->type == 2) || (mmap_entry->type == 4) || (mmap_entry->type == 5)) {
-            printf("used\n");
-            mark_used(base_addr, length);
+            filter(base_addr, length);
         }
 
         // Add memory map entry size + size field
         mmap_entry = (mmap_entry_t *)((uintptr_t)mmap_entry + mmap_entry->size + sizeof(mmap_entry->size));
     }
-
-    mark_used((uintptr_t)&kernel_start, (uintptr_t)&kernel_end - (uintptr_t)&kernel_start); // Kernel, includes kernel stack and PMM bitmap
-    mark_used((uintptr_t)multiboot_info_ptr, sizeof(multiboot_info_t));                     // MBI struct
-    mark_used(mbi->mmap_addr, mbi->mmap_length);                                            // Memory map buffer
-    mark_used(0xB80000, 8000);                                                              // VGA memory
-    // TODO: Mark areas used by boot modules (mods_*)
     
+    /* TODO: 
+        Pass address of command line to kernel
+        Mark areas used by boot modules (mods_*)
+        Other multiboot info needed
+    */
+
     // TODO: Unmap the identiy mapping of the first 4 MiB of memory
 }
 
